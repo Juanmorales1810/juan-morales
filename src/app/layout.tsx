3fa60c81dd6ab01a1e2cade209691031314bfda5
Juanmorales1810 <juan1810.jm@gmail.com>
2025-04-30 12:48:32 -0300

    Añadir el componente RootLayout con soporte para temas y configuración de metadatos

---
+++ src/app/layout.tsx
@@ -0,0 +1,59 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata, Viewport } from "next";
+import { siteConfig } from "@/config/site";
+
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const viewport: Viewport = {
+    themeColor: [
+        { color: "#f3e8ff", media: "(prefers-color-scheme: light)" },
+        { color: "#18181b", media: "(prefers-color-scheme: dark)" },
+    ],
+};
+
+export const metadata: Metadata = {
+    title: {
+        default: siteConfig.name,
+        template: `%s - ${siteConfig.name}`,
+    },
+    description: siteConfig.description,
+    icons: {
+        icon: "/favicon.svg",
+        shortcut: "/favicon-16x16.png",
+        apple: "/apple-touch-icon.png",
+    },
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="es" suppressHydrationWarning>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased relative flex flex-col w-full bg-gradient-to-br from-purple-100 via-fuchsia-100 to-violet-200 dark:bg-gradient-to-br dark:from-zinc-900 dark:via-zinc-950 dark:to-neutral-900 `}
+            >
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                >
+                    <div className="absolute z-0 h-full w-full bg-[radial-gradient(#d4d4d8_1px,transparent_1px)] dark:bg-[radial-gradient(#27272a_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
