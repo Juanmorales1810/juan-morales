4bfb8295264bafe581a3608338f51f4b481ab50f
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-05-01 02:16:28 -0300

    Añadir componente Header al layout principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Geist, Geist_Mono } from "next/font/google";
 import type { Metadata, Viewport } from "next";
+import { Header } from "@/components/navbar";
 import { siteConfig } from "@/config/site";
 
 import "@/styles/globals.css";
@@ -51,6 +52,7 @@ export default function RootLayout({
                     enableSystem
                 >
                     <div className="absolute z-0 h-full w-full bg-[radial-gradient(#d4d4d8_1px,transparent_1px)] dark:bg-[radial-gradient(#27272a_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
+                    <Header />
                     {children}
                 </ThemeProvider>
             </body>
