b78cd7a3f69344803927fd5157a99aecdb0e2d29
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-05-01 02:17:53 -0300

    Añadir componente Header con navegación para escritorio y móvil

---
+++ src/components/navbar.tsx
@@ -0,0 +1,83 @@
+"use client";
+import {
+    Navbar,
+    NavBody,
+    NavItems,
+    MobileNav,
+    NavbarLogo,
+    NavbarButton,
+    MobileNavHeader,
+    MobileNavToggle,
+    MobileNavMenu,
+} from "@/components/resizable-navbar";
+import { useState } from "react";
+import { ModeToggle } from "./toggle-mode";
+
+export function Header() {
+    const navItems = [
+        { name: "Inicio", link: "#Inicio" },
+        {
+            name: "Experiencia",
+            link: "#Experiencia",
+        },
+        { name: "Bio", link: "#Bio" },
+        { name: "Proyectos", link: "#Proyectos" },
+        { name: "Contacto", link: "#Contacto" },
+    ];
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    return (
+        <Navbar>
+            {/* Desktop Navigation */}
+            <NavBody>
+                <NavbarLogo />
+                <NavItems items={navItems} />
+                <div className="flex items-center gap-4">
+                    <NavbarButton variant="primary">Contacto</NavbarButton>
+                    <ModeToggle />
+                </div>
+            </NavBody>
+
+            {/* Mobile Navigation */}
+            <MobileNav>
+                <MobileNavHeader>
+                    <NavbarLogo />
+                    <div className="flex items-center gap-4">
+                        <ModeToggle />
+                        <MobileNavToggle
+                            isOpen={isMobileMenuOpen}
+                            onClick={() =>
+                                setIsMobileMenuOpen(!isMobileMenuOpen)
+                            }
+                        />
+                    </div>
+                </MobileNavHeader>
+
+                <MobileNavMenu
+                    isOpen={isMobileMenuOpen}
+                    onClose={() => setIsMobileMenuOpen(false)}
+                >
+                    {navItems.map((item, idx) => (
+                        <a
+                            key={`mobile-link-${idx}`}
+                            href={item.link}
+                            onClick={() => setIsMobileMenuOpen(false)}
+                            className="relative text-neutral-600 dark:text-neutral-300"
+                        >
+                            <span className="block">{item.name}</span>
+                        </a>
+                    ))}
+                    <div className="flex w-full flex-col gap-4">
+                        <NavbarButton
+                            onClick={() => setIsMobileMenuOpen(false)}
+                            variant="primary"
+                            className="w-full"
+                        >
+                            Contacto
+                        </NavbarButton>
+                    </div>
+                </MobileNavMenu>
+            </MobileNav>
+        </Navbar>
+    );
+}
