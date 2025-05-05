6c398c9b2f67ae16e0a2855573a6fda2f11e9007
Juanmorales1810 <juan1810.jm@gmail.com>
2025-05-05 12:31:54 -0300

    Agregar importación del componente Experiencia en page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import Contact from "@/components/interfaces/contact";
 import Hero from "@/components/interfaces/hero";
 import Cta from "@/components/interfaces/cta";
 import Bio from "@/components/interfaces/bio";
+import Experiencia from "@/components/interfaces/experiencia";
 
 export default function Home() {
     return (
         <main className="relative flex w-full flex-col items-center justify-center gap-4">
             <Hero />
+            {/* <Experiencia /> */}
             <Experience />
             <Bio />
             <Proyects />
