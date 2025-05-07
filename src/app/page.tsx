701693b8a58908fb08c4f19ac0857a1dee795f25
Juanmorales1810 <juan1810.jm@gmail.com>
2025-05-07 12:49:06 -0300

    Corregir importación del componente Experiencia y restaurar su uso en la página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import Experience from "@/components/interfaces/experience";
+import Experiencia from "@/components/interfaces/experiencia";
 import Proyects from "@/components/interfaces/proyects";
 import Contact from "@/components/interfaces/contact";
 import Hero from "@/components/interfaces/hero";
 import Cta from "@/components/interfaces/cta";
 import Bio from "@/components/interfaces/bio";
-import Experiencia from "@/components/interfaces/experiencia";
+import SobreMi from "@/components/interfaces/sobre-mi";
 
 export default function Home() {
     return (
         <main className="relative flex w-full flex-col items-center justify-center gap-4">
             <Hero />
-            {/* <Experiencia /> */}
-            <Experience />
-            <Bio />
+            <Experiencia />
+            <SobreMi />
             <Proyects />
             <Cta />
             <Contact />
